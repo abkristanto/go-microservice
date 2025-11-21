f160b2e32d23f6f0d5fe58ade31079c98a5587ad
Abraham Kristanto <abkristanto@gmail.com>
2025-11-21 13:17:30 +1100

    Add ExternalID in provider event DTO and remove ID

--- internal/providers/dtos/event.go
+++ internal/providers/dtos/event.go
@@ -3,7 +3,7 @@ package dtos
 import "time"
 
 type Event struct {
-	ID          string    `json:"id"`
+	ExternalID  string    `json:"id"`
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
 	StartsAt    time.Time `json:"starts_at"`
